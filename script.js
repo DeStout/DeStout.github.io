8fb732a5a97edd80fbb0eeae3c013c9e753f22ca
De Stout <destoutdev@gmail.com>
2019-01-08 22:49:18 -0500

    Update and simplify code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+document.getElementById("homePanel").classList.add("visible");
+
 var blades = document.getElementsByClassName("blade");
 var panels = document.getElementsByClassName("panel");
 
 for(let blade of blades) {
     blade.addEventListener("click", function() {
-
-        this.parentElement.style.float = "left";
         
         for(let panel of panels) {
             panel.classList.remove("visible");
         }
-        this.nextElementSibling.classList.add("visible");
+        var panel = this.nextElementSibling;
+        panel.classList.add("visible");
     });
 }
